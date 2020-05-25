00b13b2a60ec22db64a6fcbd010ba571db8d176a
abhudaym <abhuday.mishra@hotmail.com>
2020-05-25 19:56:07 +0530

    Added Comments routes

--- models/Post.js
+++ models/Post.js
@@ -29,7 +29,7 @@ const PostSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'users',
       },
-      type: {
+      text: {
         type: String,
         required: true,
       },
