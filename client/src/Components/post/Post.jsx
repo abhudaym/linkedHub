d12c234f066a5b6bdd37c70b20f17cf6070be998
Abhuday Mishra <abhuday@Abhudays-MacBook-Pro.local>
2020-06-23 14:52:21 +0530

    Comment posting error

--- client/src/Components/post/Post.jsx
+++ client/src/Components/post/Post.jsx
@@ -1,16 +1,21 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect, Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import { getPost } from '../../actions/post';
 import { connect } from 'react-redux';
 import Spinner from '../Layout/Spinner';
+import CommentForm from './CommentForm';
+import { addComment } from '../../actions/post';
 
-const Post = ({ getPost, post: {post}, match }) => {
+const Post = ({ getPost, post: { post, loading }, match, addComment }) => {
   useEffect(() => {
     getPost(match.params.id);
   }, [getPost, match.params.id]);
 
-console.log(post.name)
-  return (
+  const [text, setText] = useState('');
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
     <section className='container'>
       <a href='posts.html' className='btn'>
         Back To Posts
@@ -18,64 +23,58 @@ console.log(post.name)
       <div className='post bg-white p-1 my-1'>
         <div>
           <a href='profile.html'>
-            <img
-              className='round-img'
-              src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200'
-              alt=''
-            />
-            <h4>John Doe</h4>
+            <img className='round-img' src={post.avatar} alt='' />
+            <h4>{post.name}</h4>
           </a>
         </div>
         <div>
-          <p className='my-1'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-            possimus corporis sunt necessitatibus! Minus nesciunt soluta
-            suscipit nobis. Amet accusamus distinctio cupiditate blanditiis
-            dolor? Illo perferendis eveniet cum cupiditate aliquam?
-          </p>
+          <p className='my-1'>{post.text}</p>
         </div>
       </div>
 
       <div className='post-form'>
         <div className='bg-primary p'>
-          <h3>Leave A Comment</h3>
+          <h3>Leave a Comment</h3>
         </div>
-        <form className='form my-1'>
+        <form
+          className='form my-1'
+          onSubmit={(e) => {
+            e.preventDefault();
+            addComment(post._id, { text });
+            setText('');
+          }}>
           <textarea
             name='text'
             cols='30'
             rows='5'
-            placeholder='Comment on this post'
+            placeholder='Create a post'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
             required></textarea>
           <input type='submit' className='btn btn-dark my-1' value='Submit' />
         </form>
       </div>
 
-      <div className='comments'>
-        <div className='post bg-white p-1 my-1'>
-          <div>
-            <a href='profile.html'>
-              <img
-                className='round-img'
-                src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200'
-                alt=''
-              />
-              <h4>John Doe</h4>
-            </a>
-          </div>
-          <div>
-            <p className='my-1'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-              possimus corporis sunt necessitatibus! Minus nesciunt soluta
-              suscipit nobis. Amet accusamus distinctio cupiditate blanditiis
-              dolor? Illo perferendis eveniet cum cupiditate aliquam?
-            </p>
-            <p className='post-date'>Posted on 04/16/2019</p>
+      {post.comments.map((comment) => (
+        <div className='comments'>
+          <div className='post bg-white p-1 my-1'>
+            <div>
+              <a href='profile.html'>
+                <img
+                  className='round-img'
+                  src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200'
+                  alt=''
+                />
+                <h4>John Doe</h4>
+              </a>
+            </div>
+            <div>
+              <p className='my-1'>{comment.text}</p>
+              <p className='post-date'>Posted on 04/16/2019</p>
+            </div>
           </div>
         </div>
-
-       
-      </div>
+      ))}
     </section>
   );
 };
@@ -89,4 +88,4 @@ const mapStateToProps = (state) => ({
   post: state.post,
 });
 
-export default connect(mapStateToProps, { getPost })(Post);
+export default connect(mapStateToProps, { getPost, addComment })(Post);
--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,7 +6,9 @@ import {
     UPDATE_LIKES,
     DELETE_POST,
     ADD_POST,
-    GET_POST
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from './types';
 import { set } from 'mongoose';
 
@@ -122,3 +124,51 @@ export const getPost = (id) => async dispatch => {
         });
     }
 }
+
+
+// Add Comment
+
+export const addComment = (postId,formData) => async dispatch => {
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    try {
+        const res = await axios.post(`/api/posts/comment/${postId}` , formData, config);
+
+        dispatch({
+            type: ADD_COMMENT,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Comment Added', 'success'));
+    } catch (error) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {msg: error.response.statusText, status: error.response.status}
+        });
+    }
+}
+
+// Remove Comment
+
+export const deleteComment = (postId,commentId) => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+        dispatch({
+            type: REMOVE_COMMENT,
+            payload: commentId
+        });
+
+        dispatch(setAlert('Comment Deleted', 'success'));
+    } catch (error) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {msg: error.response.statusText, status: error.response.status}
+        });
+    }
+}
+
